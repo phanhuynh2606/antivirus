c1fa59bdde828c986dc0b1e29fec07b3e70ecf7b
Huynh Phan <96413938+phanhuynh2606@users.noreply.github.com>
2023-07-04 15:36:32 +0700

    Add files via upload

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,11 +1,23 @@
 
-   const videoContainer = document.getElementById('watchvideo');
-   const playButton = document.querySelector('.button-play');
+const videoContainer = document.getElementById('watchvideo');
+const playButton = document.querySelector('.button-play');
 
-   playButton.addEventListener('click', function() {
-      videoContainer.classList.add('active');
-   });
+playButton.addEventListener('click', function () {
+   videoContainer.classList.add('active');
+});
 
-   videoContainer.addEventListener('click', function() {
-      videoContainer.classList.remove('active');
-   });
+videoContainer.addEventListener('click', function () {
+   videoContainer.classList.remove('active');
+});
+
+// Lấy phần tử HTML liên quan đến nút điều hướng
+var toggleButton = document.querySelector(".inner-menu-mobi .button-menu");
+
+// Lấy phần tử HTML liên quan đến menu
+var menu = document.querySelector("#show-menu");
+
+// Thêm sự kiện "click" vào nút điều hướng
+toggleButton.addEventListener("click", function() {
+  // Thêm hoặc xóa lớp "show-menu" vào phần tử HTML liên quan đến menu
+  menu.classList.toggle("show-menu");
+});
